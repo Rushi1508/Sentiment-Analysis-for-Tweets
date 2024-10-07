8c6d9aa593ef70a8b608dd682b9d30e55ed70e8f
Rushi Parikh <126984217+Rushi1508@users.noreply.github.com>
2024-10-07 10:49:12 +0530

    Updated module

---
+++ preprocessing.py
@@ -0,0 +1,54 @@
+import re
+import string
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+
+def lemmatize_sentence(tokens):
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_sentence = [lemmatizer.lemmatize(word, get_wordnet_pos(tag)) for word, tag in pos_tag(tokens)]
+    return lemmatized_sentence
+
+def get_wordnet_pos(pos_tag):
+    if pos_tag.startswith('N'):
+        return 'n'
+    elif pos_tag.startswith('V'):
+        return 'v'
+    elif pos_tag.startswith('R'):
+        return 'r'
+    else:
+        return 'a'
+
+def remove_noise(tweet_tokens, stop_words=()):
+    cleaned_tokens = []
+
+    for token, tag in pos_tag(tweet_tokens):
+        token = re.sub(r'http[s]?://\S+', '', token)
+        token = re.sub(r"@\w+", "", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+
+    return cleaned_tokens
+
+def get_all_words(cleaned_tokens_list):
+    for tokens in cleaned_tokens_list:
+        for token in tokens:
+            yield token
+
+def get_tweets_for_model(cleaned_tokens_list):
+    for tweet_tokens in cleaned_tokens_list:
+        yield dict([token, True] for token in tweet_tokens)
+
+stop_words = stopwords.words('english')
